0d492be81d868b833bb525288035f11bfdabeed1
rupesh-dev30 <cgcrupesh@gmail.com>
2024-04-21 08:38:44 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import Header from "./components/Header.jsx";
+import UserInput from "./components/UserInput.jsx";
+import Results from "./components/Results.jsx";
+
+function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  }
+
+  const inputIsValid = userInput.duration >= 1;
+
+  return (
+    <>
+      <Header />
+      <UserInput userInput={userInput} onChange={handleChange}/>
+      {!inputIsValid && <p className="center">Please enter a duration greater than zero.</p>}
+      {inputIsValid && <Results userInput={userInput} />}
+    </>
+  );
+}
+
+export default App;
